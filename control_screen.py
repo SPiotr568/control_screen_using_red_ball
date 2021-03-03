12be600f4e1a974c79253e39512f922187a1227b
SPiotr568 <piotrsularz99@gmail.com>
2021-03-03 15:53:13 +0100

    add files

---
+++ control_screen.py
@@ -0,0 +1,47 @@
+import cv2 
+import numpy as np
+import pyautogui
+
+camera = cv2.VideoCapture(0)
+
+red_floor = np.array([140, 90, 120], np.uint8)
+red_ceil = np.array([180, 255, 255], np.uint8)
+
+last_y = 0
+last_x = 0
+
+def scroll(y):
+    diff = last_y - y
+    if abs(diff) > 10:
+        pyautogui.scroll((diff-10)/3)
+
+def change_tab(x):
+    diff = last_x - x
+    if diff > 150:
+        pyautogui.hotkey('ctrl', 'tab')
+
+while True:
+    ret, frame = camera.read()
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    red_mask = cv2.inRange(hsv_frame, red_floor, red_ceil)
+    contours, hierarchy = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for c in contours:
+        area = cv2.contourArea(c)
+        if area > 800:
+            x, y, w, h = cv2.boundingRect(c)
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+            cv2.drawContours(frame, c, -1, (0,0,255), 2)
+            scroll(y)
+            change_tab(x)
+            last_y = y
+            last_x = x
+
+    cv2.imshow('Screen controller',frame)
+    cv2.imshow('Red mask',red_mask)
+
+    if cv2.waitKey(10) == ord('q'):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
